b362d33c15b3c63587a80368b7ac70131b8a8e58
Lonami Exo <totufals@hotmail.com>
2020-01-16 13:01:40 +0100

    Type hint client/secrets

--- telethon/client/secret.py
+++ telethon/client/secret.py
@@ -2,7 +2,9 @@ import random
 from hashlib import sha1, sha256
 from time import time
 
-from .. import utils
+import typing
+
+from .. import utils, helpers, hints
 from ..crypto import AES
 from ..errors import SecurityError, EncryptionAlreadyDeclinedError
 from ..extensions import BinaryReader
@@ -10,21 +12,27 @@ from ..network.mtprotostate import MTProtoState
 from ..tl.functions.messages import AcceptEncryptionRequest
 from ..tl.functions.messages import GetDhConfigRequest, RequestEncryptionRequest, SendEncryptedServiceRequest, \
     DiscardEncryptionRequest, SendEncryptedRequest
-from ..tl.types import InputEncryptedChat, TypeEncryptedChat
+from ..tl.types import InputEncryptedChat, TypeEncryptedChat, EncryptedChat
 from ..tl.types.messages import DhConfigNotModified
 from ..tl.types.secret import *
+
+
+if typing.TYPE_CHECKING:
+    from .telegramclient import TelegramClient
+    from ..tl.types import TypeDecryptedMessage
+
 
 DEFAULT_LAYER = 101
 
 
 class ChatKey:
-    def __init__(self, auth_key: bytes):
+    def __init__(self: 'TelegramClient', auth_key: bytes):
         self.auth_key = auth_key
         self.fingerprint = None
 
 
 class Chats:
-    def __init__(self, id: int, access_hash: int, key: ChatKey, admin: bool, user_id: int,
+    def __init__(self: 'TelegramClient', id: int, access_hash: int, key: ChatKey, admin: bool, user_id: int,
                  input_chat: InputEncryptedChat):
         self.id = id
         self.access_hash = access_hash
@@ -49,7 +57,7 @@ class Chats:
 
 class SecretChatMethods:
 
-    def get_secret_chat(self, chat_id) -> Chats:
+    def get_secret_chat(self: 'TelegramClient', chat_id: 'typing.Union[Chats, hints.EntityLike]') -> Chats:
         if isinstance(chat_id, int):
             peer = self.secret_chats.get(chat_id, None)
             if not peer:
@@ -71,7 +79,7 @@ class SecretChatMethods:
             pass
         raise ValueError("chat not found")
 
-    async def get_dh_config(self):
+    async def get_dh_config(self: 'TelegramClient'):
         version = 0 if not self.dh_config else self.dh_config.version
         dh_config = await self(GetDhConfigRequest(random_length=0, version=version))
         if isinstance(dh_config, DhConfigNotModified):
@@ -80,14 +88,14 @@ class SecretChatMethods:
         self.dh_config = dh_config
         return dh_config
 
-    def check_g_a(self, g_a: int, p: int) -> bool:
+    def check_g_a(self: 'TelegramClient', g_a: int, p: int) -> bool:
         if g_a <= 1 or g_a >= p - 1:
             raise ValueError("g_a is invalid (1 < g_a < p - 1 is false).")
         if g_a < 2 ** 1984 or g_a >= p - 2 ** 1984:
             raise ValueError("g_a is invalid (1 < g_a < p - 1 is false).")
         return True
 
-    async def start_secret_chat(self, peer):
+    async def start_secret_chat(self: 'TelegramClient', peer: 'hints.EntityLike'):
         peer = utils.get_input_user(await self.get_input_entity(peer))
         dh_config = await self.get_dh_config()
         a = int.from_bytes(os.urandom(256), 'big', signed=False)
@@ -97,13 +105,13 @@ class SecretChatMethods:
         self.temp_secret_chat[res.id] = a
         return res.id
 
-    def generate_secret_in_seq_no(self, chat_id):
+    def generate_secret_in_seq_no(self: 'TelegramClient', chat_id: int):
         return self.secret_chats[chat_id].in_seq_no * 2 + self.secret_chats[chat_id].in_seq_no_x
 
-    def generate_secret_out_seq_no(self, chat_id):
+    def generate_secret_out_seq_no(self: 'TelegramClient', chat_id: int):
         return self.secret_chats[chat_id].out_seq_no * 2 + self.secret_chats[chat_id].out_seq_no_x
 
-    async def rekey(self, peer):
+    async def rekey(self: 'TelegramClient', peer: Chats):
         peer = self.get_secret_chat(peer)
         self._log.debug(f'Rekeying secret chat {peer}')
         dh_config = await self.get_dh_config()
@@ -122,7 +130,7 @@ class SecretChatMethods:
 
         return e
 
-    async def accept_rekey(self, peer, action: DecryptedMessageActionRequestKey):
+    async def accept_rekey(self: 'TelegramClient', peer: Chats, action: DecryptedMessageActionRequestKey):
         peer = self.get_secret_chat(peer)
         if peer.rekeying[0] != 0:
             my_exchange_id = peer.rekeying[1]
@@ -154,7 +162,7 @@ class SecretChatMethods:
         message = await self.encrypt_secret_message(peer, message)
         await self(SendEncryptedServiceRequest(InputEncryptedChat(peer.id, peer.access_hash), message))
 
-    async def commit_rekey(self, peer, action: DecryptedMessageActionAcceptKey):
+    async def commit_rekey(self: 'TelegramClient', peer: Chats, action: DecryptedMessageActionAcceptKey):
         peer = self.get_secret_chat(peer)
         if peer.rekeying[0] != 1 or not self.temp_rekeyed_secret_chats.get(action.exchange_id, None):
             peer.rekeying = [0]
@@ -186,7 +194,7 @@ class SecretChatMethods:
         peer.ttl = 100
         peer.updated = time()
 
-    async def complete_rekey(self, peer, action: DecryptedMessageActionCommitKey):
+    async def complete_rekey(self: 'TelegramClient', peer: Chats, action: DecryptedMessageActionCommitKey):
         peer = self.get_secret_chat(peer)
         if peer.rekeying[0] != 2 or self.temp_rekeyed_secret_chats.get(action.exchange_id, None):
             return
@@ -209,7 +217,7 @@ class SecretChatMethods:
         await self(SendEncryptedServiceRequest(InputEncryptedChat(peer.id, peer.access_hash), message))
         self._log.debug(f'Secret chat {peer} rekeyed succrsfully')
 
-    async def handle_decrypted_message(self, decrypted_message, peer: Chats):
+    async def handle_decrypted_message(self: 'TelegramClient', decrypted_message, peer: Chats):
         if isinstance(decrypted_message, (DecryptedMessageService, DecryptedMessageService8)):
             if isinstance(decrypted_message.action, DecryptedMessageActionRequestKey):
                 await self.accept_rekey(peer, decrypted_message.action)
@@ -257,7 +265,7 @@ class SecretChatMethods:
             decrypted_message = decrypted_message.message
             return await self.handle_decrypted_message(decrypted_message, peer)
 
-    async def handle_encrypted_update(self, event):
+    async def handle_encrypted_update(self: 'TelegramClient', event):
         if not self.secret_chats.get(event.message.chat_id):
             self._log.debug("Secret chat not saved. skipping")
             return False
@@ -297,7 +305,7 @@ class SecretChatMethods:
         peer.incoming[peer.in_seq_no] = message
         return await self.handle_decrypted_message(decrypted_message, peer)
 
-    async def encrypt_secret_message(self, peer, message):
+    async def encrypt_secret_message(self: 'TelegramClient', peer: Chats, message: TypeDecryptedMessage):
         peer = self.get_secret_chat(peer)
         peer.ttr -= 1
         if peer.layer > 8:
@@ -335,7 +343,12 @@ class SecretChatMethods:
                                                                                           aes_iv)
         return message
 
-    async def send_secret_message(self, peer_id, message, ttl=0, reply_to_id=None):
+    async def send_secret_message(
+            self: 'TelegramClient',
+            peer_id: 'hints.EntityLike',
+            message: str,
+            ttl: int = 0,
+            reply_to_id: int = None):
         peer = self.get_secret_chat(peer_id)
         if peer.layer == 8:
             message = DecryptedMessage8(os.urandom(8), message, DecryptedMessageMediaEmpty())
@@ -348,7 +361,7 @@ class SecretChatMethods:
             SendEncryptedRequest(peer=peer.input_chat, data=data))
         return res
 
-    async def notify_layer(self, peer):
+    async def notify_layer(self: 'TelegramClient', peer: hints.EntityLike):
         if isinstance(peer, int):
             peer = self.secret_chats[peer]
         else:
@@ -362,7 +375,7 @@ class SecretChatMethods:
             SendEncryptedServiceRequest(peer=InputEncryptedChat(peer.id, peer.access_hash),
                                         data=data))
 
-    async def close_secret_chat(self, peer):
+    async def close_secret_chat(self: 'TelegramClient', peer: Chats):
 
         if self.secret_chats.get(peer.id, None):
             del self.secret_chats[peer]
@@ -373,7 +386,7 @@ class SecretChatMethods:
         except EncryptionAlreadyDeclinedError:
             pass
 
-    def decrypt_mtproto2(self, message_key, chat_id, encrypted_data):
+    def decrypt_mtproto2(self: 'TelegramClient', message_key: bytes, chat_id: int, encrypted_data: bytes):
         peer = self.get_secret_chat(chat_id)
 
         aes_key, aes_iv = MTProtoState._calc_key(self.secret_chats[chat_id].key.auth_key,
@@ -397,7 +410,7 @@ class SecretChatMethods:
 
         return BinaryReader(message_data).tgread_object()
 
-    def decrypt_mtproto1(self, message_key, chat_id, encrypted_data):
+    def decrypt_mtproto1(self: 'TelegramClient', message_key: bytes, chat_id: int, encrypted_data: bytes):
         aes_key, aes_iv = MTProtoState._old_calc_key(self.secret_chats[chat_id].key.auth_key,
                                                      message_key,
                                                      True)
@@ -416,7 +429,7 @@ class SecretChatMethods:
 
         return BinaryReader(message_data).tgread_object()
 
-    async def accept_secret_chat(self, chat: TypeEncryptedChat):
+    async def accept_secret_chat(self: 'TelegramClient', chat: TypeEncryptedChat):
         if chat.id == 0:
             raise ValueError("Already accepted")
         dh_config = await self.get_dh_config()
@@ -439,7 +452,7 @@ class SecretChatMethods:
         await self.notify_layer(chat)
         return result
 
-    async def finish_secret_chat_creation(self, chat):
+    async def finish_secret_chat_creation(self: 'TelegramClient', chat: EncryptedChat):
         dh_config = await self.get_dh_config()
         g_a_or_b = int.from_bytes(chat.g_a_or_b, "big", signed=False)
         self.check_g_a(g_a_or_b, dh_config.p)
